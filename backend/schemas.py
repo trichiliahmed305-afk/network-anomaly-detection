30f74b747e8cad490e0799ed06b0829aa414f627
Ahmed Trichili <your-email@example.com>
2026-05-09 21:35:44 +0200

    Fix: add model field to TrafficData schema

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,19 +1,19 @@
-from pydantic import BaseModel
+﻿from pydantic import BaseModel
 
 class TrafficData(BaseModel):
     id_orig_p: float = 0.0           # Port source
     id_resp_p: float = 80.0          # Port destination
-    duration: float = 0.0            # Durée connexion
-    orig_bytes: float = 0.0          # Octets envoyés
-    resp_bytes: float = 0.0          # Octets reçus
+    duration: float = 0.0            # DurÃ©e connexion
+    orig_bytes: float = 0.0          # Octets envoyÃ©s
+    resp_bytes: float = 0.0          # Octets reÃ§us
     missed_bytes: float = 0.0        # Octets perdus
-    orig_pkts: float = 1.0           # Paquets envoyés
+    orig_pkts: float = 1.0           # Paquets envoyÃ©s
     orig_ip_bytes: float = 0.0       # Octets IP source
-    resp_pkts: float = 1.0           # Paquets reçus
+    resp_pkts: float = 1.0           # Paquets reÃ§us
     resp_ip_bytes: float = 0.0       # Octets IP destination
     is_orig_local: int = 1           # IP locale (1) ou externe (0)
-    orig_h_count: float = 1.0        # Fréquence IP source
-    resp_h_count: float = 1.0        # Fréquence IP destination
+    orig_h_count: float = 1.0        # FrÃ©quence IP source
+    resp_h_count: float = 1.0        # FrÃ©quence IP destination
     is_well_known_port: int = 0      # Port < 1024
     hour: int = 12                   # Heure
     minute: int = 0                  # Minute
